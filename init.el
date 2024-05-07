49fbf146b9f5edd9ddae28462e5226a653bed0f7
Artur <artawower@mail.ru>
2024-05-07 23:10:36 +0400

    centered mode

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
 (require 'org)
 ; (org-babel-load-file (concat user-emacs-directory "README.org"))
-(org-babel-load-file "~/pure-emacs/README.org")
+(org-babel-load-file "~/apps/pure-emacs/README.org")
 
